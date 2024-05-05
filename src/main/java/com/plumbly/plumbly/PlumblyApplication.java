d3074dbb1535761c83c6e8523b170a27856d76cf
rutkaykarabulak <rutkaykarabulak@gmail.com>
2024-05-05 23:56:31 +0100

    initial commit of plumbly web app

---
+++ src/main/java/com/plumbly/plumbly/PlumblyApplication.java
@@ -0,0 +1,13 @@
+package com.plumbly.plumbly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumblyApplication.class, args);
+	}
+
+}

9df488f7bb93931c1b67251c104e29888e0df919
rutkaykarabulak <rutkaykarabulak@gmail.com>
2024-05-09 23:58:56 +0100

    fix unit tests

--- src/test/java/com/plumbly/PlumblyApplicationTests.java
+++ src/test/java/com/plumbly/PlumblyApplicationTests.java
@@ -2,10 +2,11 @@ package com.plumbly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class PlumblyApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}

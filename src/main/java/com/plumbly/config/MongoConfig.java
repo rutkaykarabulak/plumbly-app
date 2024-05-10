346c73ca3967e609cb0d5d1a8114e2bf18801333
rutkaykarabulak <rutkaykarabulak@gmail.com>
2024-05-10 01:26:18 +0100

    improvements on mongodb config and application properties

--- src/main/java/com/plumbly/config/MongoConfig.java
+++ src/main/java/com/plumbly/config/MongoConfig.java
@@ -3,16 +3,52 @@ package com.plumbly.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.plumbly.models.User;
+
+import java.util.Collections;
+
 
 @Configuration
-public class MongoConfig {
-    @Value("${mongodbConnectionUrl}") 
-    String connectionUrl;
+public class MongoConfig extends AbstractMongoClientConfiguration{
+    @Value("${mongodb.connection.url}") 
+    private String connectionUrl;
+    
+    @Value ("${mongodb.database.name}")
+    private String databaseName;
+
+    @Value ("${mongodb.host}")
+    private String host;
+
+    @Value ("${mongodb.port}")
+    private int port;
+    
+    @Override
+    public String getDatabaseName() {
+        return databaseName;
+    }
+    
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder
+                .applicationName("plumbly")
+                .applyConnectionString(new ConnectionString(connectionUrl))
+                .applyToClusterSettings(
+                        settings -> settings.hosts(Collections.singletonList(new ServerAddress(host, port))))
+                .build();
+    }
+
+    @Override
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create(connectionUrl);
+        MongoClient mongoClient = super.mongoClient();
+        return mongoClient;
     }
+
 }
